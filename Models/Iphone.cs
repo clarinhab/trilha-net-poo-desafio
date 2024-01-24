4ef0175d995c92fb00e9d77824171089a41afffe
ClarinhaB <128818456+clarinhab@users.noreply.github.com>
2024-01-24 13:50:33 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,9 +1,8 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
     public class iPhone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        
         public iPhone(string numero,string modelo,string imei,int memoria) : base(numero, modelo, imei, memoria)
         {
         }
